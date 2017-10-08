edde414cbf272dab7d1480ec0e8b611b7f0cd64d
raihanorium <raihan.dk@gmail.com>
2017-10-09 00:39:01 +0600

    Created static resource mapping, redirected to index.html as default file.

--- src/main/java/com/raihanorium/newsportal/controller/HomeController.java
+++ src/main/java/com/raihanorium/newsportal/controller/HomeController.java
@@ -3,15 +3,16 @@ package com.raihanorium.newsportal.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by ataul.raihan on 10/7/2017.
  */
 @RestController
-@RequestMapping(value = "/api")
+@RequestMapping
 public class HomeController {
     @RequestMapping(method = RequestMethod.GET)
-    public String showHome() {
-        return "Home";
+    public ModelAndView redirect() {
+        return new ModelAndView("redirect:/index.html");
     }
 }
